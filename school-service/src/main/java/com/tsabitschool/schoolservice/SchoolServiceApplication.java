510369ace4c24d66c72a4b68e67205fa5cf9a541
tsabit ghazwan <tsabit.ghazwan@thetreedots.com>
2024-03-29 00:13:51 +0700

    fix: add eureka client

--- school-service/src/main/java/com/tsabitschool/schoolservice/SchoolServiceApplication.java
+++ school-service/src/main/java/com/tsabitschool/schoolservice/SchoolServiceApplication.java
@@ -5,11 +5,13 @@ import com.tsabitschool.schoolservice.repository.SchoolRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import java.util.Optional;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SchoolServiceApplication {
 
 	public static void main(String[] args) {
